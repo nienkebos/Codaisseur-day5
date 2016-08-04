8f31545fa98ee59e92a7a342183a0c02fb85ef63
nienkebos <nienkebos@live.com>
2016-08-04 11:32:52 +0200

    New contact action method

--- Tacta.rb
+++ Tacta.rb
@@ -23,6 +23,19 @@ def ask(prompt)
   gets.chomp
 end
 
+def create_new
+  contact = {}
+
+  puts
+  puts "Enter contact info"
+
+  contact[:name]  = ask "Name? "
+  contact[:phone] = ask "Phone? "
+  contact[:email] = ask "Email? "
+
+  contact
+end
+
 loop do
   index(contacts)
 
@@ -32,14 +45,8 @@ loop do
   break if response == "q"
 
   if response == "n"
-    contact = {}
-
-    puts
-    puts "Enter contact info"
-
-    contact[:name]  = ask "Name? "
-    contact[:phone] = ask "Phone? "
-    contact[:email] = ask "Email? "
+    
+    contact = create_new
 
     contacts << contact
 
