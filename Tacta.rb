6f078a4930c03690b1bb2aff1affcc98dd7c5556
nienkebos <nienkebos@live.com>
2016-08-04 12:03:48 +0200

    Read and write to contacts file

--- Tacta.rb
+++ Tacta.rb
@@ -1,3 +1,16 @@
+require 'json'
+
+def read_contacts
+  json = File.read('contacts.json')
+  array = JSON.parse(json,{:symbolize_names => true})
+end
+
+def write_contacts(contacts)
+  File.open("contacts.json", "w") do |f|
+    json = JSON.pretty_generate(contacts)
+    f.write(json)
+  end
+end
 
 def index(contacts)
   contacts.each_with_index do |contact, i|
