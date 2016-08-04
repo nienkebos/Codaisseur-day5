4f5a8cbd7c20db64e029833463e5284eabcff8a9
nienkebos <nienkebos@live.com>
2016-08-04 11:16:32 +0200

    Use loop to stay in the program

--- Tacta.rb
+++ Tacta.rb
@@ -23,12 +23,18 @@ def ask(prompt)
   gets.chomp
 end
 
-puts
-response = aks "Who would you like to see? "
+loop do
+  index(contacts)
 
-i = response.to_i
+  puts
+  response = ask "Who would you like to see? "
 
-contact = contacts[i-1]
+  i = response.to_i
 
-puts
-show(contact)
+  contact = contacts[i-1]
+
+  puts
+  show(contact)
+  puts
+
+end
