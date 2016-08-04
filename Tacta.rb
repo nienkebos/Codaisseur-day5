8f9f31eccb79523b881ac438c499088b0ca091b8
nienkebos <nienkebos@live.com>
2016-08-04 11:55:35 +0200

    Search contacts

--- Tacta.rb
+++ Tacta.rb
@@ -77,18 +77,32 @@ def action_error
   puts
 end
 
+def action_search(contacts)
+  puts
+  pattern = ask "Search for? "
+  puts
+
+  contacts.each do |contact|
+    if contact[:name] =~ /\b#{pattern}/i
+      show(contact)
+      puts
+    end
+  end
+end
 
 loop do
   index(contacts)
 
   puts
-  response = ask "Who would you like to see (n for new, d for delete, q to quit)? "
+  response = ask "Who would you like to see (n for new, d for delete, s for search, q to quit)? "
 
   break if response == "q"
   if response == "n"
     action_new(contacts)
   elsif response == "d"
     action_delete(contacts)
+  elsif response == "s"
+    action_search(contacts)
   elsif response =~ /[0-9]+/
     action_show(contacts, response.to_i)
   else
